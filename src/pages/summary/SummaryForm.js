691ee669f3b6ebd67a02cf35f19c6a31bfda35bc
Kth <vcro12123@naver.com>
2022-08-27 16:12:57 +0900

    체크박스 테스트

--- src/pages/summary/SummaryForm.js
+++ src/pages/summary/SummaryForm.js
@@ -1,3 +1,26 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 export default function SummatForm() {
-  return <div />;
+  const [tuChecked, setTcChecked] = useState(false);
+
+  const checkBoxLable = (
+    <span>
+      I agree to <span style={{ color: 'blue' }}>terms and conditions</span>
+    </span>
+  );
+
+  return (
+    <Form>
+      <Form.Group controlId='terms-and-conditions'>
+        <Form.Check
+          type='checkbox'
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkBoxLable}
+        ></Form.Check>
+      </Form.Group>
+      <Button variant={'primary'} type='submit' disabled={!tuChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
 }
