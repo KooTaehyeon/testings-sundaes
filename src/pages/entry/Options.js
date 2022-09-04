92803aa07da79984b69dc7d2d9824dbf8e676434
Kth <vcro12123@naver.com>
2022-09-04 17:43:26 +0900

    options,scoopoption setting

--- src/pages/entry/Options.js
+++ src/pages/entry/Options.js
@@ -1,7 +1,26 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ScoopOption from './ScoopOption';
+import { Row } from 'react-bootstrap';
 const Options = ({ optionType }) => {
-  return <div></div>;
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {});
+  }, [optionType]);
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={items.name}
+      name={items.name}
+      imagePath={items.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
 };
 
 export default Options;
